6ea5f689192328d5ac2972788ef9b683b0884f7f
Daniel Berger <K12005210@students.jku.at>
2023-01-03 16:38:48 +0100

    Right-clicking the Lure Stick now attracts mobs. Implementation is currently very hacky, as they do now willingly walk towards the player but are teleported veeery often for veeery small distances.

--- LureStick/src/main/java/com/github/Bengonator/better_luring/init/ItemsInit.java
+++ LureStick/src/main/java/com/github/Bengonator/better_luring/init/ItemsInit.java
@@ -1,18 +1,26 @@
 package com.github.Bengonator.better_luring.init;
 
 import com.github.Bengonator.better_luring.BetterLuring;
+import com.github.Bengonator.better_luring.items.LureStick;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-public class ItemInit {
+public class ItemsInit {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BetterLuring.MODID);
 
 	// todo testen ob leerzeichen erlaubt sind im namen
 	public static final RegistryObject<Item> LURE_STICK = ITEMS.register("lure_stick",
-			() -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+			() -> new LureStick((new Item.Properties()
+					.tab(CreativeModeTab.TAB_TOOLS)
+//					.durability(64)
+					.defaultDurability(100) // todo was das?
+//					.craftRemainder() // wie bucket bei lava. wenn i zb an stick mach den ma mit zb essen befüllen muss
+					// dann kann i machen, dass ma den leeren stick griagt sobald de durability weg is
+					// natürli muss i dann crafting rezepte machen und ah schaun wegen dem, dass mei gegenstand ned breakt.
+			)));
 
 
 }
