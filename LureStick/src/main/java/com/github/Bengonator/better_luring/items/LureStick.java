a870a4a28d9a27f67d59533b89ff93f115feb5d6
Daniel Berger <K12005210@students.jku.at>
2023-01-11 12:26:32 +0100

    Not choosing any item name (=filter), now automatically selects all mobs. Changed the position where the mobs go to, from the players position, to the place the player is looking at, when using the item. Sound is removed and a particle effect is used instead. The effect work fine most of the time, apart when it is being used on a side if a block which is not the top. Did a bit of reordering of the code, and added regions and comments for better reading and understanding.

--- LureStick/src/main/java/com/github/Bengonator/better_luring/items/LureStick.java
+++ LureStick/src/main/java/com/github/Bengonator/better_luring/items/LureStick.java
@@ -2,24 +2,38 @@ package com.github.Bengonator.better_luring.items;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.particle.SimpleAnimatedParticle;
 import net.minecraft.commands.arguments.EntityAnchorArgument;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.*;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.entity.ai.goal.WrappedGoal;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.npc.WanderingTrader;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -34,43 +48,110 @@ public class LureStick extends Item {
 	@Override
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
 
-		player.getCooldowns().addCooldown(this, ticksPerSec * duration); // todo +1 vllt
+		// + 1, so that there is some safety time between two uses of the item.
+		player.getCooldowns().addCooldown(this, ticksPerSec * duration + 1);
 		ItemStack itemStack = player.getItemInHand(interactionHand);
-		int dmg = itemStack.getDamageValue();
+		int takenDmg = itemStack.getDamageValue();
+		int remainingDmg = itemStack.getMaxDamage() - takenDmg;
 
-		if (!level.isClientSide()) {
-			if (itemStack.getMaxDamage() - dmg <= 0) {
+		double playerX = player.getX();
+		double playerY = player.getY();
+		double playerZ = player.getZ();
+
+		BlockHitResult hitResult = getPlayerPOVHitResult(level, player, ClipContext.Fluid.NONE);
+		double hitX = hitResult.getLocation().x;
+		double hitY = hitResult.getLocation().y;
+		double hitZ = hitResult.getLocation().z;
+
+		if (level.isClientSide) {
+			if (remainingDmg > 0) {
+
+				// todo wenn block drüber
+				//  was wenn seitlich oder sogar unten berühren?
+				//  wie mit non solids umgehen? einfach so wie jetzt, also innen spawnen?
+//				BlockState blockState = level.getBlockState(hitResult);
+				double offsetForTopOfBlock = 0;
+//				if (blockState.isSolidRender(level, hitResult)) {
+//					offsetForTopOfBlock = blockState.getShape(level, hitResult).max(Direction.Axis.Y, 0.5, 0.5) + 0.03125;
+//				}
+
+				Random rnd = new Random();
+				float spread = 0.2F;
+//				rnd.nextFloat(-spread, +spread)
+
+				RandomSource randomsource = level.getRandom();
+				for(int times = 0; times < 5; times++) {
+					for (int i = 0; i < 5; i++) {
+
+						level.addParticle(
+							ParticleTypes.COMPOSTER,
+							hitX + i * rnd.nextFloat(-spread, +spread),
+							hitY + i * 0.2,
+							hitZ + i * rnd.nextFloat(-spread, +spread),
+							randomsource.nextGaussian() * 0.02,
+							randomsource.nextGaussian() * 0.02,
+							randomsource.nextGaussian() * 0.02);
+					}
+				}
+			}
+			else {
 				player.playSound(SoundEvents.ITEM_BREAK, .5F, 1F);
-
-			} else {
-				// todo damit ma ned vom sound weglaufen kann, vllt lieber was kurzes nehmen. besonders weil dann ah ka problem is mit längere durations
-				//  vllt kurzer sound und dafür partikel während duration, somit keine probleme mit duration, und afoch partikel mitgeben, die an der blockpos gesetzt werden
-				// todo is sound nu da? habn nie gehört. aber lieber wegtun und dafür partikel, markeirt somit wo mobs hingehen
-				player.playSound(SoundEvents.PORTAL_AMBIENT, 1F, 1F); // todo sound entfernen? oder eben was kurzes machen
-				itemStack.setDamageValue(dmg + 1);
+			}
+		}
+		else {
+			if (remainingDmg > 0) {
 
 				TargetingConditions tarCon = TargetingConditions.DEFAULT
 					.ignoreLineOfSight()
 					.ignoreInvisibilityTesting();
 
-				level.getNearbyEntities(
-					Mob.class, tarCon, player, player.getBoundingBox().inflate(range))
-					.forEach( mob -> {
+				int affected = 0;
+				for (Mob mob : level.getNearbyEntities(Mob.class, tarCon, player, player.getBoundingBox().inflate(range))) {
 
-					// todo amount nicht vergessen, es soll aber nur hoch gezählt werden, wenn es ein tier ist das beeinflusst wird
-					// also im bezug auf whitelist. was mit ghast, ...?? weiß nu ned
+					if (affected == amount || affected == remainingDmg) break;
 
-					// todo aus namen auslesen und danach entscheiden, welche mobs angezogen werden
-					//  vllt mit + - am anfang für black oder whitelist
-					//  dann ; trennt alles
-					//  mob.getClass().getSimpleName()
-					//  solange keine wildcards oder gruppen (friedlich, monster,...) brauch ich nur einmal + oder - checken
+					/* If there is a custom name,
+					   create the White- or Blacklist
+					   and skip this mob,
+					     if it is a Whitelist and the name is not contained
+					     or if it is a Blacklist and the name is contained.
+					*/
+					if (itemStack.hasCustomHoverName()) {
 
-					// todo vllt villagers extra beobachten
-					//  mob.getBrain().removeAllBehaviors();
-					//  mob.getBrain().setDefaultActivity(Activity.PANIC);
-					//  ghasts sind auch komiscch, vllt "alle" testen und jene einzeln debuggen und schaun ob goals exisitieren
-					//  was mit fische?
+						// region create White- or Blacklist
+						ArrayList<String> split = new ArrayList<>(List.of(itemStack.getHoverName().getString().trim().split(";")));
+
+						String start = split.get(0);
+						boolean isWhitelist = true;
+
+						if (start.charAt(0) == '+') {
+							split.set(0, start.substring(1).trim());
+
+						} else if (start.charAt(0) == '-') {
+							isWhitelist = false;
+							split.set(0, start.substring(1).trim());
+						}
+
+						for (int i = 1; i < split.size(); i++) {
+							split.set(i, split.get(i).trim());
+						}
+						// endregion create Whit- or Blacklist
+
+						boolean contains = split.contains(mob.getDisplayName().getString());
+						if ( (isWhitelist && !contains) || (!isWhitelist && contains) ) {
+							continue;
+						}
+					}
+
+					// region affect mobs
+					affected++;
+
+					/*
+					 todo vllt villagers extra beobachten
+					  mob.getBrain().removeAllBehaviors();
+					  mob.getBrain().setDefaultActivity(Activity.PANIC);
+					  ghasts sind auch komiscch, vllt "alle" testen und jene einzeln debuggen und schaun ob goals exisitieren
+*/
 
 					GoalSelector goalSelector = mob.goalSelector;
 
@@ -80,7 +161,7 @@ public class LureStick extends Item {
 					List<WrappedGoal> goals = goalSelector.getAvailableGoals().stream().toList();
 					goalSelector.removeAllGoals();
 
-					Executors.newScheduledThreadPool(1).schedule(
+					Executors.newSingleThreadScheduledExecutor().schedule(
 						() -> {
 							for (WrappedGoal goal : goals) {
 								goalSelector.addGoal(goal.getPriority(), goal.getGoal());
@@ -91,22 +172,31 @@ public class LureStick extends Item {
 
 					mob.lookAt(
 						EntityAnchorArgument.Anchor.EYES,
-						new Vec3(player.getX(), player.getY(), player.getZ()));
+						new Vec3(playerX, playerY, playerZ));
 
-					mob.getNavigation().moveTo(player, speedModifier);
-				});
+					mob.getNavigation().moveTo(hitX, hitY, hitZ, speedModifier);
+
+					itemStack.setDamageValue(takenDmg + affected);
+					// endregion affect mobs
+				}
 			}
 		}
 
 		return super.use(level, player, interactionHand);
 	}
 
+/*	@Override
+	public InteractionResult interactLivingEntity(ItemStack p_41398_, Player p_41399_, LivingEntity p_41400_, InteractionHand p_41401_) {
+		// todo wenn i später das machen will mit tiereauswählen per rechts click und wegwählen per shift rechts click oder so
+
+		return super.interactLivingEntity(p_41398_, p_41399_, p_41400_, p_41401_);
+	}*/
+
 	@Override
 	public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag) {
 
-		// todo testen ob beide shift tasten
 		if (Screen.hasShiftDown()) {
-			components.add(Component.literal("right-click to make entities move towards you"));
+			components.add(Component.literal("right-click to lure mobs"));
 		}
 
 		super.appendHoverText(itemStack, level, components, tooltipFlag);
