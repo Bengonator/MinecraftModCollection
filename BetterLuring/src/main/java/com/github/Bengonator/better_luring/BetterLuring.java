4245a87cee16b6f178e494dde808fdb92c167315
Daniel Berger <39526272+Bengonator@users.noreply.github.com>
2023-02-28 09:12:38 +0100

    Updated Forge
    Updated from 1.19.2-43.2.0 to 1.19.3-44.1.0

--- BetterLuring/src/main/java/com/github/Bengonator/better_luring/BetterLuring.java
+++ BetterLuring/src/main/java/com/github/Bengonator/better_luring/BetterLuring.java
@@ -3,7 +3,12 @@ package com.github.Bengonator.better_luring;
 import com.github.Bengonator.better_luring.inits.BlocksInit;
 import com.github.Bengonator.better_luring.inits.EnchantmentsInit;
 import com.github.Bengonator.better_luring.inits.ItemsInit;
+import com.github.Bengonator.better_luring.items.LureStick;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -24,6 +29,20 @@ public class BetterLuring {
         BlocksInit.BLOCK_ENTITIES.register(modEventBus);
         EnchantmentsInit.ENCHANTMENTS.register(modEventBus);
 
+        modEventBus.addListener(this::buildCreativeTabContents);
+
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void buildCreativeTabContents(CreativeModeTabEvent.BuildContents event) {
+
+        CreativeModeTab tab = event.getTab();
+
+        if (tab == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(new ItemStack(ItemsInit.LURE_STICK.get()));
+
+        } else if (tab == CreativeModeTabs.REDSTONE_BLOCKS) {
+            event.accept(new ItemStack(BlocksInit.LURE_BLOCK_ITEM.get()));
+        }
     }
 }
