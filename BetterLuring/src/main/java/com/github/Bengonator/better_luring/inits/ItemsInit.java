3608922697da12b5d43ef0bab9e1311f16043c2e
Daniel Berger <39526272+Bengonator@users.noreply.github.com>
2023-07-05 09:38:09 +0200

    Updated to 1.19.4 and removed unused folders and files
    added new 1.19.4 mod Jar and remove folders regarding BetterHoppers and the file which explained git rebase and the file for lfs which is not used anymore

--- BetterLuring/src/main/java/com/github/Bengonator/better_luring/inits/ItemsInit.java
+++ BetterLuring/src/main/java/com/github/Bengonator/better_luring/inits/ItemsInit.java
@@ -2,7 +2,6 @@ package com.github.Bengonator.better_luring.inits;
 
 import com.github.Bengonator.better_luring.BetterLuring;
 import com.github.Bengonator.better_luring.items.LureStick;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -10,7 +9,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ItemsInit {
 
-	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BetterLuring.MODID);
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BetterLuring.MOD_ID);
 
 	public static final RegistryObject<Item> LURE_STICK = ITEMS.register("lure_stick", () ->
 		new LureStick(new Item.Properties()
